9fbbef5ac05dfca818af6238926011ee92aa5d06
FireSquid <peterkreyn@gmail.com>
2022-01-28 20:11:30 -0800

    Added a xamarin forms template project and a gitignore (#1)

---
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LimbPreservationTool.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LimbPreservationTool.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

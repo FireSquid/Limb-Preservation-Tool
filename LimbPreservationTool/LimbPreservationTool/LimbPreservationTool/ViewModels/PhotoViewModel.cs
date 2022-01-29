d913f07d41ce8824cd0e7a68dfc942d242de02b6
FireSquid <peterkreyn@gmail.com>
2022-01-28 22:55:11 -0800

    Added pictures page and camera functionality.
    Added additional page with the ability to access the device's camera for taking pictures. The picture is then displayed in the app. This new page can be found under the "Pictures" option in the flyout menu.

---
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace LimbPreservationTool.ViewModels
+{
+    public class PhotoViewModel : BaseViewModel
+    {
+        public PhotoViewModel()
+        {
+            Title = "About";
+            PictureStatus = "No Picture Found";
+            TakePhotoCommand = new Command(async () => await GetImageFromCamera());
+        }
+
+        private async Task<ImageSource> TakePhoto()
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+            {
+                ImageSource nextPhoto = ImageSource.FromStream(() => { return photo.GetStream(); });
+                
+                return nextPhoto;
+            }
+            return null;
+        }
+
+        private async Task GetImageFromCamera()
+        {
+            ImageSource newImage = await TakePhoto();
+
+            if (newImage != null)
+            {
+                PictureStatus = $"Found Picture, {newImage.ToString()}";
+                LastPhoto = newImage;
+            }
+            
+        }
+
+        public ICommand TakePhotoCommand { get; }
+
+        private ImageSource lastPhoto;
+        public ImageSource LastPhoto { get => lastPhoto; set => SetProperty(ref lastPhoto, value); }
+
+        private string pictureStatus;
+        public string PictureStatus { get => pictureStatus; set => SetProperty(ref pictureStatus, value); }
+    }
+}

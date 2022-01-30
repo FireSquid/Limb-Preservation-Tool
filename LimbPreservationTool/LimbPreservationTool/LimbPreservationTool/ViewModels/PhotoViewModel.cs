8b86d3a177943e074037b06fecf0c0c6294c4a22
ReggieSean <yxuhuang@ucdavis.edu>
2022-01-29 17:42:43 -0800

    using Xamarin.Essentials

--- LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/PhotoViewModel.cs
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/PhotoViewModel.cs
@@ -10,6 +10,7 @@ using Xamarin.Essentials;
 
 using System.Windows.Input;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LimbPreservationTool.ViewModels
 {
@@ -20,30 +21,87 @@ namespace LimbPreservationTool.ViewModels
             Title = "About";
             PictureStatus = "No Picture Found";
             TakePhotoCommand = new Command(async () => await GetImageFromCamera());
+            		    
+            System.Diagnostics.Debug.WriteLine("Photo ViewModel\n");
         }
 
-        private async Task<ImageSource> TakePhoto()
-        {
-            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+        //private async Task<ImageSource> TakePhoto()
+        //{
+        //    var photo = await Media.CrossMedia.Current.TakePhotoAsync(new Media.Abstractions.StoreCameraMediaOptions() { });
 
-            if (photo != null)
+        //    if (photo != null)
+        //    {
+        //        ImageSource nextPhoto = ImageSource.FromStream(() => { return photo.GetStream(); });
+        //        
+        //        return nextPhoto;
+        //    }
+        //    return null;
+        //}
+		private ImageSource PhotoPath;
+        async Task TakePhoto()
+        {
+            try
             {
-                ImageSource nextPhoto = ImageSource.FromStream(() => { return photo.GetStream(); });
                 
-                return nextPhoto;
+                System.Diagnostics.Debug.WriteLine("Beging Taking Photo\n");
+                //PermissionStatus status = await MediaPicker.RequestPermissionAsync<CalendarPermission>();
+                var photo = await MediaPicker.CapturePhotoAsync();
+
+                System.Diagnostics.Debug.WriteLine("Photo Capatured\n");
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
             }
-            return null;
+            catch (FeatureNotSupportedException fnsEx)
+            {
+
+                System.Diagnostics.Debug.WriteLine("Feature Not Supported\n");
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+
+                System.Diagnostics.Debug.WriteLine("Permission Denied\n");
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+
+                System.Diagnostics.Debug.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                System.Diagnostics.Debug.WriteLine("Null Photo\n");
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+
+            Console.Write("Photo Taken");
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
         }
 
         private async Task GetImageFromCamera()
         {
-            ImageSource newImage = await TakePhoto();
+            //ImageSource newImage = await TakePhoto();
 
-            if (newImage != null)
-            {
-                PictureStatus = $"Found Picture, {newImage.ToString()}";
-                LastPhoto = newImage;
-            }
+            //if (newImage != null)
+            //{
+            //    PictureStatus = $"Found Picture, {newImage.ToString()}";
+            //    LastPhoto = newImage;
+            //}
+
+			await TakePhoto();
+		    LastPhoto = PhotoPath;	
             
         }
 

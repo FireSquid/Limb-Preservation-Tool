ebae236b833e72f04f901adce9427912d3deb8fa
FireSquid <peterkreyn@gmail.com>
2022-02-22 00:48:39 -0800

    Added wifi tool page that can be navigated to from the home page

--- LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/HomeViewModel.cs
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/HomeViewModel.cs
@@ -15,6 +15,7 @@ namespace LimbPreservationTool.ViewModels
         {
             Title = "Home";
             TakeNewPhotoCommand = new Command(async () => await TakeNewPhoto());
+            EnterAdditionalInfoCommand = new Command(async () => await EnterAdditionalWifiInfo());
         }
 
         async Task TakeNewPhoto()
@@ -22,6 +23,13 @@ namespace LimbPreservationTool.ViewModels
             await Shell.Current.GoToAsync($"//{nameof(PhotoPage)}");
         }
 
+        async Task EnterAdditionalWifiInfo()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(WifiPage)}");
+        }
+
         public ICommand TakeNewPhotoCommand { get; }
+
+        public ICommand EnterAdditionalInfoCommand { get; }
     }
 }

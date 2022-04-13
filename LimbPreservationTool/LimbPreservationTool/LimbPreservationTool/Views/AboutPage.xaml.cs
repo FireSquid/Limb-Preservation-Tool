a67932178bbee7901a20f080e989e6c39448cb0a
mkjost0 <50555529+mkjost0@users.noreply.github.com>
2022-04-13 14:14:55 -0700

    Additional UI changes, started about page but isn't yet functional

--- LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/Views/AboutPage.xaml.cs
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/Views/AboutPage.xaml.cs
@@ -3,13 +3,22 @@ using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LimbPreservationTool.ViewModels;
+
 namespace LimbPreservationTool.Views
 {
     public partial class AboutPage : ContentPage
     {
+
+        AboutViewModel viewModel;
+
         public AboutPage()
         {
             InitializeComponent();
+            viewModel = new AboutViewModel();
+            this.BindingContext = viewModel;
         }
     }
 }

f738944100c6ca9ad9309026cc27bdbd80819abf
mkjost0 <50555529+mkjost0@users.noreply.github.com>
2022-04-19 13:35:44 -0700

    Fixed about page, now links back to home

--- LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/AboutViewModel.cs
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/AboutViewModel.cs
@@ -3,6 +3,9 @@ using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
+using LimbPreservationTool.Views;
+
+
 
 namespace LimbPreservationTool.ViewModels
 {
@@ -12,8 +15,11 @@ namespace LimbPreservationTool.ViewModels
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            BacktoHome = new Command(async () => await Shell.Current.GoToAsync($"//{nameof(HomePage)}"));
         }
 
         public ICommand OpenWebCommand { get; }
+
+        public ICommand BacktoHome { get; }
     }
 }

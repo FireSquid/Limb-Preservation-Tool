b1638627f3a1467149ff393af971f06dae90b2e6
unknown <gcook65@yahoo.com>
2022-05-03 21:54:19 -0700

    commented out possibly unnecessary lines

--- LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/NewUserViewModel.cs
+++ LimbPreservationTool/LimbPreservationTool/LimbPreservationTool/ViewModels/NewUserViewModel.cs
@@ -19,7 +19,7 @@ namespace LimbPreservationTool.ViewModels
             //ResetLoginMessage();
             //LoginCommand = new Command(OnLoginClicked);
             CreateUserCommand = new Command(OnCreateUserClicked);
-            CreatedAccountCommand = new Command(async () => await FinalizeAccount());
+            //CreatedAccountCommand = new Command(async () => await FinalizeAccount());
         }
 
         private async void OnCreateUserClicked(object obj)
@@ -48,50 +48,13 @@ namespace LimbPreservationTool.ViewModels
 
         private string creationStatus;
         public string CreationStatus { get => creationStatus; private set => SetProperty(ref creationStatus, value); }
-        async Task FinalizeAccount()
+        /*async Task FinalizeAccount()
         {
             await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
-        }
-
+        }*/
         public ICommand CreatedAccountCommand { get; }
 
         public Command CreateUserCommand { get; }
-
-        /*private async void OnLoginClicked(object obj)
-        {
-            LoginStatus = "Authenticating Login Information...";
-            //if (await VerifyLoginEntry())
-            if (true)
-            {
-                LoginStatus = "Login Successful";
-                await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
-            }
-            else
-            {
-                LoginStatus = $"Login Failed - Invalid Username or Password";
-            }
-        }
-
-        public void ResetLoginMessage()
-        {
-            LoginStatus = "Awaiting Login";
-        }
-
-        public void ClearLoginInformation()
-        {
-            UsernameEntryField = "";
-            PasswordEntryField = "";
-        }
-
-        public Command LoginCommand { get; }
-
-        private string usernameEntryField;
-        public string UsernameEntryField { get => usernameEntryField; set => SetProperty(ref usernameEntryField, value); }
-
-        private string passwordEntryField;
-        public string PasswordEntryField { get => passwordEntryField; set => SetProperty(ref passwordEntryField, value); }
-
-        private string loginStatus;
-        public string LoginStatus { get => loginStatus; private set => SetProperty(ref loginStatus, value); }*/
+     
     }
 }
